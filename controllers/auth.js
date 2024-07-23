a542afa78bb0a60b7eb141b7d9773bea6afd3e0c
Qasim <qalhamad33@gmail.com>
2024-07-23 12:14:16 +0300

    add initial validation for sign up

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,33 @@
 const express = require("express");
 const router = express.Router();
 
+const User = require("../models/user.js");
+
 module.exports = router;
 
 router.get('/sign-up', (req, res , next) => {
     res.render('auth/sign-up.ejs')
 })
+
+router.post("/sign-up", async (req, res) => {
+
+    const username = req.body.username;
+    const password = req.body.password;
+    const confirmPassword = req.body.confirmPassword;
+    try {
+        const existingUser = await User.findOne({
+            username,
+        })
+        if(existingUser){
+            return res.send('Oops something went wrong')
+        }
+
+        if(password !== confirmPassword){   
+            return res.send('Password and Confirm Password do not match');
+        }
+
+
+    } catch (error) {
+        
+    }
+  });
